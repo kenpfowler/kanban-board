7ab6096b558a8a6c06ac6edc299fabe9aa31a44c
Ken <development@kennyf.ca>
2022-03-13 03:00:50 -0400

    made columns draggabel and added highlighing before drop

--- src/components/column/column.component.jsx
+++ src/components/column/column.component.jsx
@@ -1,65 +1,79 @@
 import React, { useState } from 'react';
 import './column.styles.css';
 import Item from '../item/item.component';
-import { Droppable } from 'react-beautiful-dnd';
+import { Droppable, Draggable } from 'react-beautiful-dnd';
 import {
   useKanbanBoardContext,
   useKanbanBoardUpdateContext,
 } from '../../KanbanBoardContext';
 
-const Column = ({ title, itemIds, columnId }) => {
+const Column = ({ title, itemIds, columnId, index }) => {
   const state = useKanbanBoardContext();
   const { addItem } = useKanbanBoardUpdateContext();
   const [input, setInput] = useState('');
 
   return (
-    <div className="column">
-      <h1 className="column__title">{title}</h1>
-      <Droppable droppableId={columnId}>
-        {(provided, snapshot) => (
-          <div
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-            className="column__container"
-          >
-            {itemIds.map((itemId, index) => {
-              return (
-                <Item
-                  key={itemId}
-                  index={index}
-                  content={state.items[itemId].content}
-                  columnId={columnId}
-                  itemId={state.items[itemId].id}
-                />
-              );
-            })}
-            {provided.placeholder}
-          </div>
-        )}
-      </Droppable>
-      <div className="column__input">
-        <input
-          type="text"
-          placeholder="Describe your task..."
-          value={input}
-          onChange={(e) => {
-            setInput(e.target.value);
-          }}
-        />
-        <button
-          className="btn btn-primary"
-          onClick={() => {
-            if (!input) {
-              return;
-            }
-            addItem({ input: input, column_id: columnId });
-            setInput('');
-          }}
+    <Draggable draggableId={columnId} index={index}>
+      {(provided) => (
+        <div
+          {...provided.draggableProps}
+          ref={provided.innerRef}
+          className="column"
         >
-          Add Task
-        </button>
-      </div>
-    </div>
+          <h1 {...provided.dragHandleProps} className="column__title">
+            {title}
+          </h1>
+          <Droppable droppableId={columnId} type="item">
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+                className={
+                  snapshot.isDraggingOver
+                    ? 'column__container column__dragging'
+                    : 'column__container'
+                }
+              >
+                {itemIds.map((itemId, index) => {
+                  return (
+                    <Item
+                      key={itemId}
+                      index={index}
+                      content={state.items[itemId].content}
+                      columnId={columnId}
+                      itemId={state.items[itemId].id}
+                    />
+                  );
+                })}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+          <div className="column__input">
+            <input
+              type="text"
+              placeholder="Describe your task..."
+              value={input}
+              onChange={(e) => {
+                setInput(e.target.value);
+              }}
+            />
+            <button
+              className="btn btn-primary"
+              onClick={() => {
+                if (!input) {
+                  return;
+                }
+                addItem({ input: input, column_id: columnId });
+                setInput('');
+              }}
+            >
+              Add Task
+            </button>
+          </div>
+        </div>
+      )}
+    </Draggable>
   );
 };
 
