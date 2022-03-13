dccef99ff51f398ed30168b5df571be8c2ede285
Ken <development@kennyf.ca>
2022-03-13 00:08:58 -0500

    refactor messy provider file

--- src/KanbanBoardContext.js
+++ src/KanbanBoardContext.js
@@ -1,13 +1,11 @@
-import React, { useReducer, useContext } from 'react';
+import React, { useContext } from 'react';
 import initialData from './initial-data';
+import { reducer } from './store/reducer';
+import { ACTIONS } from './store/actions';
+import { useReducerWithLogger } from './hooks/useReducerWithLogger.hook.js';
 
 const KanbanBoardContext = React.createContext();
 const KanbanBoardUpdateContext = React.createContext();
-
-const ACTIONS = {
-  ADD_COLUMN: 'ADD_COLUMN',
-  ADD_ITEM: 'ADD_ITEM',
-};
 
 export const useKanbanBoardContext = () => {
   return useContext(KanbanBoardContext);
@@ -17,44 +15,25 @@ export const useKanbanBoardUpdateContext = () => {
   return useContext(KanbanBoardUpdateContext);
 };
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case ACTIONS.ADD_COLUMN:
-      const columnIdNumber = state.columnOrder.push(
-        `column-${state.columnOrder.length + 1}`
-      );
-      const columnId = `column-${columnIdNumber}`;
-
-      return {
-        ...state,
-        columns: {
-          ...state.columns,
-          [columnId]: { id: columnId, title: action.payload, itemIds: [] },
-        },
-      };
-    default:
-      return state;
-  }
-};
-
 export const KanbanBoardProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialData);
+  const [state, dispatch] = useReducerWithLogger(reducer, initialData);
 
   const addColumn = (payload) => {
-    console.log(`Dispached ${ACTIONS.ADD_COLUMN}!`);
     dispatch({ type: ACTIONS.ADD_COLUMN, payload: payload });
   };
 
   const addItem = (payload) => {
-    console.log(`Dispached ${ACTIONS.ADD_ITEM}!`);
-    dispatch();
-    //add an item with the correct key, id, and content to the state
-    //add the item to the correct column in the state
+    dispatch({ type: ACTIONS.ADD_ITEM, payload: payload });
+  };
+
+  const moveItem = (payload) => {
+    dispatch({ type: ACTIONS.MOVE_ITEM, payload: payload });
   };
 
   const dispatchFunctions = {
     addColumn: addColumn,
     addItem: addItem,
+    moveItem: moveItem,
   };
 
   return (
