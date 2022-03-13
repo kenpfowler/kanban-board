68bffe04ec4cd0e022cd40489838c36cf4cd362b
Ken <development@kennyf.ca>
2022-03-13 00:04:49 -0500

    create dummy data, dummy database files, and initialize some dummy data

--- src/initial-data.js
+++ src/initial-data.js
@@ -4,12 +4,13 @@ const initialData = {
     'item-2': { id: 'item-2', content: 'Cleaning the hosue' },
     'item-3': { id: 'item-3', content: 'Cooking' },
     'item-4': { id: 'item-4', content: 'Blogging' },
+    'item-5': { id: 'item-5', content: 'Howl at the Moon' },
   },
   columns: {
     'column-1': {
       id: 'column-1',
       title: "To Do's",
-      itemIds: ['item-1', 'item-2', 'item-3', 'item-4'],
+      itemIds: ['item-1', 'item-2', 'item-3', 'item-4', 'item-5'],
     },
   },
   columnOrder: ['column-1'],
