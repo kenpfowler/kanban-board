3a3b89e52af5c9ecde521ac4323b61de5069d61e
Ken <development@kennyf.ca>
2022-03-13 03:02:58 -0400

    added starting data

--- src/initial-data.js
+++ src/initial-data.js
@@ -12,8 +12,18 @@ const initialData = {
       title: "To Do's",
       itemIds: ['item-1', 'item-2', 'item-3', 'item-4', 'item-5'],
     },
+    'column-2': {
+      id: 'column-2',
+      title: 'In Progress',
+      itemIds: [],
+    },
+    'column-3': {
+      id: 'column-3',
+      title: 'Done',
+      itemIds: [],
+    },
   },
-  columnOrder: ['column-1'],
+  columnOrder: ['column-1', 'column-2', 'column-3'],
 };
 
 export default initialData;
