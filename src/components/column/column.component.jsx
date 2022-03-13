db2c136616358ca86d544c8b1f858a0abcc77342
Ken <development@kennyf.ca>
2022-03-13 00:07:33 -0500

    create droppable column

--- src/components/column/column.component.jsx
+++ src/components/column/column.component.jsx
@@ -1,37 +1,63 @@
 import React, { useState } from 'react';
 import './column.styles.css';
-
 import Item from '../item/item.component';
-import { useKanbanBoardContext } from '../../KanbanBoardContext';
+import { Droppable } from 'react-beautiful-dnd';
+import {
+  useKanbanBoardContext,
+  useKanbanBoardUpdateContext,
+} from '../../KanbanBoardContext';
 
-const Column = ({ title, itemIds }) => {
+const Column = ({ title, itemIds, columnId }) => {
   const state = useKanbanBoardContext();
+  const { addItem } = useKanbanBoardUpdateContext();
   const [input, setInput] = useState('');
 
   return (
     <div className="column">
       <h1 className="column__title">{title}</h1>
-      <div className="column__container">
-        {itemIds.map((itemId) => {
-          console.log(state.items[itemId]);
-          return (
-            <Item
-              key={state.items[itemId].id}
-              content={state.items[itemId].content}
-            />
-          );
-        })}
-      </div>
+      <Droppable droppableId={columnId}>
+        {(provided, snapshot) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            className="column__container"
+          >
+            {itemIds.map((itemId, index) => {
+              return (
+                <Item
+                  key={itemId}
+                  index={index}
+                  content={state.items[itemId].content}
+                  columnId={columnId}
+                  itemId={state.items[itemId].id}
+                />
+              );
+            })}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
       <div className="column__input">
         <input
           type="text"
-          placeholder="add your next task"
+          placeholder="Describe your task..."
           value={input}
           onChange={(e) => {
             setInput(e.target.value);
           }}
         />
-        <button onClick={() => {}}>+ Add</button>
+        <button
+          className="btn btn-primary"
+          onClick={() => {
+            if (!input) {
+              return;
+            }
+            addItem({ input: input, column_id: columnId });
+            setInput('');
+          }}
+        >
+          Add Task
+        </button>
       </div>
     </div>
   );
