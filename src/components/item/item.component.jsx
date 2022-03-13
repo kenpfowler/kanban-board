785bf18ef6cb684d965371095e02a02ea025f289
Ken <development@kennyf.ca>
2022-03-13 00:07:49 -0500

    create droppable items

--- src/components/item/item.component.jsx
+++ src/components/item/item.component.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import './item.styles.css';
+import { Draggable } from 'react-beautiful-dnd';
 
-const Item = ({ id, content }) => {
+const Item = ({ index, content, removeItem, itemId, columnId }) => {
   return (
-    <div key={id} className="column__item">
-      {content}
-    </div>
+    <Draggable draggableId={itemId} index={index}>
+      {(provided, snapshot) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          className="column__item"
+        >
+          {content}
+        </div>
+      )}
+    </Draggable>
   );
 };
 
