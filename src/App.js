e0c9354c0a76e8f1bc3556d90372dab58879b581
Ken <development@kennyf.ca>
2022-03-13 00:08:34 -0500

    make application drag and dropabble and add dispatch an action

--- src/App.js
+++ src/App.js
@@ -1,51 +1,103 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import Column from './components/column/column.component';
+import { DragDropContext } from 'react-beautiful-dnd';
 import {
   useKanbanBoardContext,
   useKanbanBoardUpdateContext,
 } from './KanbanBoardContext';
-
+import ReactIcon from './icons/react.icon';
 function App() {
   const [title, setTitle] = useState('');
   const state = useKanbanBoardContext();
-  const { addColumn } = useKanbanBoardUpdateContext();
-  console.log(state);
+  const { addColumn, moveItem } = useKanbanBoardUpdateContext();
+  // using useCallback is optional
+  const onBeforeCapture = useCallback(() => {
+    /*...*/
+  }, []);
+  const onBeforeDragStart = useCallback(() => {
+    /*...*/
+  }, []);
+  const onDragStart = useCallback(() => {
+    /*...*/
+  }, []);
+  const onDragUpdate = useCallback(() => {
+    /*...*/
+  }, []);
+
+  const onDragEnd = useCallback(
+    (result) => {
+      // the only one that is required
+      const { destination, source } = result;
+      if (!destination) {
+        return;
+      }
+
+      if (
+        destination.droppableId === source.droppableId &&
+        destination.index === source.index
+      ) {
+        return;
+      }
+
+      moveItem(result);
+    },
+    [moveItem]
+  );
 
   return (
-    <div className="container">
-      <header>
-        <h1>Kanban Board</h1>
-        <div>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
-          />
-          <button
-            onClick={() => {
-              addColumn(title);
-              setTitle('');
-            }}
-          >
-            Create New List
-          </button>
+    <DragDropContext
+      onBeforeCapture={onBeforeCapture}
+      onBeforeDragStart={onBeforeDragStart}
+      onDragStart={onDragStart}
+      onDragUpdate={onDragUpdate}
+      onDragEnd={onDragEnd}
+    >
+      <div className="container">
+        <header>
+          <div className="header__logo-container">
+            <ReactIcon />
+            <h1>Kanban Board</h1>
+          </div>
+          <div>
+            <div className="column__input">
+              <input
+                type="text"
+                placeholder="Give your board a title..."
+                value={title}
+                onChange={(e) => {
+                  setTitle(e.target.value);
+                }}
+              />
+              <button
+                className="btn button-secondary"
+                onClick={() => {
+                  if (!title) {
+                    return;
+                  }
+                  addColumn(title);
+                  setTitle('');
+                }}
+              >
+                Create New List
+              </button>
+            </div>
+          </div>
+        </header>
+        <div className="kanban__board">
+          {state.columnOrder.map((column) => {
+            return (
+              <Column
+                key={state.columns[column].id}
+                columnId={state.columns[column].id}
+                title={state.columns[column].title}
+                itemIds={state.columns[column].itemIds}
+              />
+            );
+          })}
         </div>
-      </header>
-      <div className="kanban__board">
-        {state.columnOrder.map((column) => {
-          return (
-            <Column
-              key={state.columns[column].id}
-              title={state.columns[column].title}
-              itemIds={state.columns[column].itemIds}
-            />
-          );
-        })}
       </div>
-    </div>
+    </DragDropContext>
   );
 }
 
